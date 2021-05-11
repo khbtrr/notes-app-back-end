5d2ce951e04645ac78cf13f71dc70efc9f4f0a5b
Kukuh Bahtiar <bahtiar.kukuh@gmail.com>
2021-05-12 06:27:08 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.9',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '13.212.190.91',
     routes: {
       cors: {
         origin: ['*'],
